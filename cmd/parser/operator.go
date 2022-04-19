1be409cc55f5a000a38ff7d1dfb76544d0a23d49
Tom Barber <tom@spicule.co.uk>
2022-04-19 17:07:46 +0000

    add sample tags

---
+++ cmd/parser/operator.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/hashicorp/hcl/v2/hclwrite"
+
+type Sink interface {
+	// Sink reads HCL and writes bytes.
+	Sink(*hclwrite.File) ([]string, error)
+}
+
+type Filter interface {
+	// Filter reads HCL and writes HCL
+	Filter(*hclwrite.File) (*hclwrite.File, error)
+}
