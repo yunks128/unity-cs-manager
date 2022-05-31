c6bbb76c5d41794788a49dcdfb057ff6a4d772ab
Tom Barber <tom@spicule.co.uk>
2022-05-31 14:47:08 +0000

    update parser

--- cmd/root.go
+++ cmd/root.go
@@ -18,7 +18,7 @@ var (
 	secgroups []string
 
 	rootCmd = &cobra.Command{
-		Use:   "cobra-cli",
+		Use:   "parse",
 		Short: "A generator for Cobra based Applications",
 		Long: `Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
--- internal/pkg/hclparser/runner.go
+++ internal/pkg/hclparser/runner.go
@@ -2,7 +2,11 @@ package hclparser
 
 import (
 	"fmt"
+	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclwrite"
+	"io/ioutil"
+	"log"
+	"strings"
 )
 
 type networkinfo struct {
@@ -17,55 +21,104 @@ type tag struct {
 func Runp(path string, tags, subnet, secgroup []string) {
 
 	fmt.Printf("hello")
+	items, _ := ioutil.ReadDir(path)
+	for _, item := range items {
+		if item.IsDir() {
+			subitems, _ := ioutil.ReadDir(item.Name())
+			for _, subitem := range subitems {
+				if !subitem.IsDir() {
+					// handle file there
+					fmt.Println(item.Name() + "/" + subitem.Name())
+					parseFile(item.Name() + "/" + subitem.Name())
+				}
+			}
+		} else {
+			// handle file there
+			fmt.Println(item.Name())
+		}
+	}
 }
 
-//func main() {
-//
-//	fbyte, err := ioutil.ReadFile("test/elastic.tf")
-//	if err != nil {
-//		fmt.Println(err)
-//	}
-//	fwr, diag := hclwrite.ParseConfig(fbyte, "example.tf", hcl.Pos{Line: 1, Column: 1})
-//	if diag.HasErrors() {
-//		fmt.Println("Couldn't parse file")
-//	}
-//	//fmt.Printf("%v", string(fwr.Bytes()))
-//	//aaf := NewAttributeAppendFilter("resource.aws_instance.unity-ec2-instance.tags.unityname", "mytag", false)
-//	//fwr, err = aaf.Filter(fwr)
-//	//err = addTagsToBlocks(fwr)
-//	blocks, err := getBlocks(fwr)
-//	if err != nil {
-//		log.Fatalf("%v", err)
-//	}
-//	for _, b := range blocks {
-//		fmt.Println(b)
-//		splits := strings.Split(b, ".")
-//		blocktype := ""
-//		if len(splits) > 1 {
-//			blocktype = splits[1]
-//		}
-//		switch blocktype {
-//		case "aws_elasticsearch_domain":
-//			//err = parseElastic(fwr, "resource.aws_elasticsearch_domain.unity-sample")
-//			err = parseElastic(fwr, blocktype)
-//			if err != nil {
-//				fmt.Printf("%v", err)
-//			}
-//		case "aws_instance":
-//			err = parseEC2(fwr, blocktype)
-//			if err != nil {
-//				fmt.Printf("%v", err)
-//			}
-//		case "":
-//			fmt.Println("three")
-//		}
-//	}
-//	fmt.Printf("%v", string(fwr.Bytes()))
-//
-//}
-
-func getFiles() {
-
+func parseFile(path string) {
+	fbyte, err := ioutil.ReadFile("test/elastic.tf")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fwr, diag := hclwrite.ParseConfig(fbyte, "example.tf", hcl.Pos{Line: 1, Column: 1})
+	if diag.HasErrors() {
+		fmt.Println("Couldn't parse file")
+	}
+	blocks, err := getBlocks(fwr)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	for _, b := range blocks {
+		fmt.Println(b)
+		splits := strings.Split(b, ".")
+		blocktype := ""
+		if len(splits) > 1 {
+			blocktype = splits[1]
+		}
+		switch blocktype {
+		case "aws_elasticsearch_domain":
+			//err = parseElastic(fwr, "resource.aws_elasticsearch_domain.unity-sample")
+			err = parseElastic(fwr, blocktype)
+			if err != nil {
+				fmt.Printf("%v", err)
+			}
+		case "aws_instance":
+			err = parseEC2(fwr, blocktype)
+			if err != nil {
+				fmt.Printf("%v", err)
+			}
+		case "":
+			fmt.Println("three")
+		}
+	}
+	//func main() {
+	//
+	//	fbyte, err := ioutil.ReadFile("test/elastic.tf")
+	//	if err != nil {
+	//		fmt.Println(err)
+	//	}
+	//	fwr, diag := hclwrite.ParseConfig(fbyte, "example.tf", hcl.Pos{Line: 1, Column: 1})
+	//	if diag.HasErrors() {
+	//		fmt.Println("Couldn't parse file")
+	//	}
+	//	//fmt.Printf("%v", string(fwr.Bytes()))
+	//	//aaf := NewAttributeAppendFilter("resource.aws_instance.unity-ec2-instance.tags.unityname", "mytag", false)
+	//	//fwr, err = aaf.Filter(fwr)
+	//	//err = addTagsToBlocks(fwr)
+	//	blocks, err := getBlocks(fwr)
+	//	if err != nil {
+	//		log.Fatalf("%v", err)
+	//	}
+	//	for _, b := range blocks {
+	//		fmt.Println(b)
+	//		splits := strings.Split(b, ".")
+	//		blocktype := ""
+	//		if len(splits) > 1 {
+	//			blocktype = splits[1]
+	//		}
+	//		switch blocktype {
+	//		case "aws_elasticsearch_domain":
+	//			//err = parseElastic(fwr, "resource.aws_elasticsearch_domain.unity-sample")
+	//			err = parseElastic(fwr, blocktype)
+	//			if err != nil {
+	//				fmt.Printf("%v", err)
+	//			}
+	//		case "aws_instance":
+	//			err = parseEC2(fwr, blocktype)
+	//			if err != nil {
+	//				fmt.Printf("%v", err)
+	//			}
+	//		case "":
+	//			fmt.Println("three")
+	//		}
+	//	}
+	//	fmt.Printf("%v", string(fwr.Bytes()))
+	//
+	//}
 }
 
 func getBlocks(f *hclwrite.File) ([]string, error) {
