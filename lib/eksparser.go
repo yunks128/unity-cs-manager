eec38c922b5af52d4e367afbf9e8c3bc28a6ba86
Barber <barber@MT-306070.jpl.nasa.gov>
2023-06-28 16:16:53 +0100

    update with new proto generator

--- lib/eksparser.go
+++ lib/eksparser.go
@@ -1,10 +1,16 @@
 package lib
 
 import (
+	"github.com/golang/protobuf/proto"
 	"github.com/unity-sds/unity-cs-manager/internal/marketplace"
 	"github.com/unity-sds/unity-cs-manager/internal/pkg/eks"
 )
 
-func GenerateEKSTemplate(model marketplace.Install_Extensions_Eks) (string, error) {
-	return eks.ProtoGenerate(model)
+func GenerateEKSTemplate(arr []byte) (string, error) {
+	model := &marketplace.Install_Extensions_Eks{}
+	err := proto.Unmarshal(arr, model)
+	if err != nil {
+		return "", err
+	}
+	return eks.ProtoGenerate(*model)
 }
